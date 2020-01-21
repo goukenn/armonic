44f5866477a75d8dbb111c1dd3a8c25997af4b11
goukenn <bondje.doue@gmail.com>
2020-01-21 12:07:31 +0100

    update 21012020

--- tst/LineTest.php
+++ tst/LineTest.php
@@ -8,22 +8,6 @@ class MMMLineTest  //+ extends AbstractArmonicTesting7
         // igk_wln_e("tesing ...");
         $data = igk_conf_load_file(dirname(__FILE__).'/Data/linetest.xml', "armonic");
         $out = igk_treat_source($data->test->input);
-        $this->assertEquals($out, $data->test->output);
-
-        // $this->runTest();
-        
-
-        // $this->runTest("");
-        // $g = new ReflectionMethod(get_class($this), "runTest");
-        
-        // usort($tc = $g->getNumberOfParameters(), function($a,$b)use(& $tab){
-        //     $tab[$a->getName()] = 1;
-        //     return 0; 
-        // });
-
-        // igk_wln("blamkd", $tc);
-
-
-        // exit;
+        $this->assertEquals($out, $data->test->output); 
     }
 }
