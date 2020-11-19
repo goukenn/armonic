4a32323c561943c9c843874a9f3ad0e5cb63c546
C.A.D. Bondje Doue <bondje.doue@gmail.com>
2020-11-19 08:52:40 +0100

    ok#

--- test_armonic.bat
+++ test_armonic.bat
@@ -1,6 +1,3 @@
-@echo OFF
-REM echo "test framework"
-REM armonic -f D:\wamp\www\igkdev\Lib\igk\igk_framework.php -of ./test/ouput.php --allowDocBlocking --verbose
+@echo OFF 
 echo "test data.php" 
-armonic -f ./test/data.php -of ./test/output.php --verbose --debug
-REM armonic -f D:\wwwroot\igkdev\Lib\igk\igk_framework.php -of ./test/output.php --allowDocBlocking
+armonic -f ./test/data.php -of ./test/output.php --verbose --debug 
